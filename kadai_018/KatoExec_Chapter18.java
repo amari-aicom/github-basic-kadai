d4210d77227833888f03e9ad4272c69fb4a6e986
amari-aicom <168692324+amari-aicom@users.noreply.github.com>
2024-05-02 14:27:14 +0900

    kadai_018のアップロード

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		KatoTaro_Chapter18 tKato = new KatoTaro_Chapter18();
+		
+		tKato.setGivenName();
+		tKato.execIntroduce();
+		
+		KatoIchiro_Chapter18 iKato = new KatoIchiro_Chapter18();
+		
+		iKato.setGivenName();
+		iKato.execIntroduce();
+		
+		KatoHanako_Chapter18 hKato = new KatoHanako_Chapter18();
+		
+		hKato.setGivenName();
+		hKato.execIntroduce();
+		
+		
+	}
+
+}
